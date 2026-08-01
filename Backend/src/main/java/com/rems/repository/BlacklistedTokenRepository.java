47d5716a00b124b4fe6143714156c7bca94729f3
AkshaySingh198 <as7708209@gmail.com>
2026-08-01 15:39:02 +0530

    Revert "Akshay singh"

--- Backend/src/main/java/com/rems/repository/BlacklistedTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rems.repository;
-
-import com.rems.entity.BlacklistedToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlacklistedTokenRepository extends JpaRepository<BlacklistedToken, Long> {
-
-    boolean existsByJti(String jti);
-}
